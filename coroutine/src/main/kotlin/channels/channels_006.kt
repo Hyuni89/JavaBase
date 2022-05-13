4ae09d7b2fcfcbe0300b258af98baf3117ec0e70
Hyuni89 <everguitarplayer@gmail.com>
2022-05-13 18:33:33 +0900

    add part of channels

---
+++ coroutine/src/main/kotlin/channels/channels_006.kt
@@ -0,0 +1,29 @@
+package me.hyuni.channels006
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.produce
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+fun CoroutineScope.produceNumbers() = produce<Int> {
+    var x = 1
+    while (true) {
+        send(x++)
+        delay(100)
+    }
+}
+
+fun CoroutineScope.launchProcessor(id: Int, channel: ReceiveChannel<Int>) = launch {
+    for (msg in channel) {
+        println("Processor #$id received $msg")
+    }
+}
+
+fun main() = runBlocking {
+    val producer = produceNumbers()
+    repeat(5) { launchProcessor(it, producer) }
+    delay(950)
+    producer.cancel()
+}
